9c4a9a83c897f78859ff815d2e20d647eda76321
Hemkar <kumarhemkar@yahoo.com>
2021-11-25 22:42:18 +0530

    add date filter and pass date back to parent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredDate, setFilteredDate] = useState("2020");
   const expenseItems = props.expenseItems;
+  const handleExpenseFilter = (date) => {
+    setFilteredDate(date);
+    console.log(filteredDate);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        filteredDate={filteredDate}
+        expenseFilterHandler={handleExpenseFilter}
+      />
       <ExpenseItem
         title={expenseItems[0].title}
         amount={expenseItems[0].amount}
