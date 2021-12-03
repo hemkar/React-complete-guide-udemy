5df6a90fc6b84d014610f8dc54bcaba2fb0f7f3e
Hemkar <kumarhemkar@yahoo.com>
2021-12-03 22:43:51 +0530

    Add logic to hide and show form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -5,29 +5,29 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  // const [userInput, setUserInput] = useState({
-  //   entereTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
-  // });
+  const [showForm, SetShowForm] = useState(false);
+
+  const handleShowForm = (e) => {
+    SetShowForm(true);
+  };
+
+  if (showForm === false) {
+    return (
+      <div text-align="right" onClick={handleShowForm}>
+        <button type="submit">Show Form</button>
+      </div>
+    );
+  }
 
   const titleChangedHandler = (event) => {
-    //setUserInput({ ...userInput, entereTitle: event.target.value });
-
-    //Use below approach if current state depends on previous state.
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
     setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    //setUserInput({ ...userInput, enteredAmount: event.target.value });
     setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    //setUserInput({ ...userInput, enteredDate: event.target.amount });
     setEnteredDate(event.target.value);
   };
 
@@ -39,7 +39,7 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    //console.log(expenseData);
+    SetShowForm(false);
     props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
